951e8abc7cada87e0a7fc2cd342cf340f4409a2d
jasnoel <jason.noel.imp@gmail.com>
2023-04-10 19:06:06 +0200

    set up reset css and tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "../app.css";
+    import Nav from './Nav.svelte';
+</script>
+
+<h1>Hey there</h1>
+<slot></slot>
+<Nav />
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,6 @@
+
+<nav class="bg-red-800">
+    <ul>
+        <li><a href="/home">Home</a></li>
+    </ul>
+</nav>
